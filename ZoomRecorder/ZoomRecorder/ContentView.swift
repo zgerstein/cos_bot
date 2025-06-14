e52a0620d1dd917514cb50c2a0af00fe180e8bc5
Zach <zgerstein@gmail.com>
2025-06-13 22:43:19 -0700

    Initial commit: Project setup with SwiftUI macOS app

---
+++ ZoomRecorder/ZoomRecorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZoomRecorder
+//
+//  Created by Zach Gerstein on 6/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZoomRecorder/ZoomRecorder/ZoomRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomRecorderApp.swift
+//  ZoomRecorder
+//
+//  Created by Zach Gerstein on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ZoomRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
